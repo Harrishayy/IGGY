a295eb0e428bbd7959301f8a5a9e8f9e77dc7cd3
Mr-Moody <118393511+Mr-Moody@users.noreply.github.com>
2025-02-05 17:00:03 +0000

    motor_board_initial

---
+++ motor.py
@@ -0,0 +1,46 @@
+import time
+
+class MotorBoard():
+    def __init__(self, arduino:object):
+        self.motor_l = Motor(arduino, 0, 0)
+        self.motor_r = Motor(arduino, 1, 0)
+
+    def forward(self, speed:int):
+        self.motor_l.set_speed(speed)
+        self.motor_r.set_speed(speed)
+
+    def backward(self, speed:int):
+        self.motor_l.set_speed(-speed)
+        self.motor_r.set_speed(-speed)
+
+    def left(self, speed:int):
+        self.motor_l.set_speed(-speed)
+        self.motor_r.set_speed(speed)
+
+    def right(self, speed:int):
+        self.motor_l.set_speed(speed)
+        self.motor_r.set_speed(-speed)
+
+    def stop(self):
+        self.motor_l.set_speed(0)
+        self.motor_r.set_speed(0)
+
+
+class Motor():
+    def __init__(self, arduino:object, id:int = 0, initial_pos:int = 0) -> None:
+        self.__arduino = arduino
+        self.__id = id
+        # ID 0 for left, 1 for right
+        
+
+    def set_speed(self, speed:int=0) -> None:
+        """
+        Set the speed of the motor
+        """
+
+        # move to position by communicating to arduino
+        string = str(self.__id) + "/" + str(speed)
+        self.__arduino.write(bytes(string, encoding="utf-8"))
+
+        message = self.__arduino.readline()
+        print("Message: ", message)
--- robot.py
+++ robot.py
@@ -2,32 +2,24 @@ import serial
 import time
 
 from leg import Leg
+from motor import MotorBoard, Motor
 
 class Robot():
     def __init__(self) -> None:
-        #self.arduino = serial.Serial(port='COM3', baudrate=9600, timeout=1)
-        self.servo_board = serial.Serial(port='COM3', baudrate=115200, timeout=1)
+        self.arduino = serial.Serial(port='COM4', baudrate=9600, timeout=1)
+        #self.servo_board = serial.Serial(port='COM3', baudrate=115200, timeout=1)
+        self.motor_board = MotorBoard(self.arduino)
 
-        self.leg_fl = Leg(self.servo_board, 2, 3, 4)
-        self.leg_fr = Leg(self.servo_board, 5, 6, 7)
-        self.leg_bl = Leg(self.servo_board, 8, 9, 10)
-        self.leg_br = Leg(self.servo_board, 11, 12, 13)
+        #self.leg_fl = Leg(self.servo_board, 2, 3, 4)
+        #self.leg_fr = Leg(self.servo_board, 5, 6, 7)
+        #self.leg_bl = Leg(self.servo_board, 8, 9, 10)
+        #self.leg_br = Leg(self.servo_board, 11, 12, 13)
 
             
     def run(self) -> None:  
-        self.leg_fl.hip_z.set_position(90)
+        self.motor_board.forward(100)
         time.sleep(1)
-        self.leg_fl.hip_z.set_position(180)
-        time.sleep(1)
-        self.leg_fl.hip_z.set_position(0)
-        time.sleep(1)
-        
-        self.leg_fl.hip_y.set_position(90)
-        time.sleep(1)
-        self.leg_fl.hip_y.set_position(180)
-        time.sleep(1)
-        self.leg_fl.hip_y.set_position(0)
-        time.sleep(1)
+        self.motor_board.stop()
 
 
 if __name__ == "__main__":
