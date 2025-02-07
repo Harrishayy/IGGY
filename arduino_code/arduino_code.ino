6cd48c60e34b187df7924c61912a7f6acb4b25d7
Mr-Moody <118393511+Mr-Moody@users.noreply.github.com>
2025-02-07 18:59:39 +0000

    LED_blinking

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,16 +1,31 @@
 
 #include <Wire.h> //communicate with I2C devices
-#include "DHT.h" //library for Digital Humidity Sensor
+//#include "DHT.h" //library for Digital Humidity Sensor
 
 
-#define DHTPIN 6
+#define DHTPIN 8
 #define DHTTYPE DHT11
 
-#define LED_PIN 3
+#define LED_PIN 2
+
+String data;
+int index = -1;
+int motor_id = 0;
+int speed = 255;
+
+//for LED blinking
+bool led_state = true;
+unsigned long loop_time = 0;
+unsigned long initial_time = 0;
+unsigned int led_interval = 500; //milliseconds
 
 
-DHT dht(DHTPIN, DHTTYPE);
+#define MOTOR_SPEED_1_PIN 7 //set pwm speed 0-255
+#define MOTOR_SPEED_2_PIN 4 //set pwm speed 0-255
+#define MOTOR_1_PIN 6 //set digital to alternate direction
+#define MOTOR_2_PIN 5
 
+//DHT dht(DHTPIN, DHTTYPE);
 
 
 void fanVoltage(float temp)
@@ -21,25 +36,102 @@ void fanVoltage(float temp)
 void setup() 
 {
   Serial.begin(115200);
-  dht.begin();
+  Serial.setTimeout(1); 
 
   pinMode(LED_PIN, OUTPUT);
 
-  digitalWrite(LED_PIN, HIGH);
-
-
-  Serial.println("");
-
+  pinMode(MOTOR_1_PIN, OUTPUT);
+  pinMode(MOTOR_2_PIN, OUTPUT);
+  pinMode(MOTOR_SPEED_1_PIN, OUTPUT);
+  pinMode(MOTOR_SPEED_2_PIN, OUTPUT);
+  
+  //dht.begin();
   
 }
 
 
 void loop() {
-  time = millis();
+  //continuous loop waiting until connection is made
+  while (!Serial.available());
 
-  float temp = dht.readTemperature();
-  float humidity = dht.readHumidity();
 
-  fanVoltage(temp);
+  //data in format "pin/position"
+  data = Serial.readString();
+
+  //find index of split between motor id and speed
+  index = data.indexOf('/');
+
+
+  if (index > -1)
+  {
+    motor_id = data.substring(0, index).toInt();
+    speed = data.substring(index + 1).toInt();
+    String strnum = String(speed);
+    Serial.print(strnum);
+
+    switch(motor_id)
+    {
+      case 0:
+        if (speed >= 0)
+        {
+          analogWrite(MOTOR_SPEED_1_PIN, speed);
+          digitalWrite(MOTOR_1_PIN, HIGH);
+        }
+        else if (speed < 0)
+        {
+          speed = -speed;
+          analogWrite(MOTOR_SPEED_1_PIN, speed);
+          digitalWrite(MOTOR_1_PIN, LOW);
+        }
+      
+        break;
+
+      case 1:
+        if (speed >= 0)
+        {
+          analogWrite(MOTOR_SPEED_2_PIN, speed);
+          digitalWrite(MOTOR_2_PIN, HIGH);
+        }
+        else if (speed < 0)
+        {
+          speed = -speed;
+          analogWrite(MOTOR_SPEED_2_PIN, speed);
+          digitalWrite(MOTOR_2_PIN, LOW);
+        }
+      
+        break;
+    }
+
+  }
+
+
+  loop_time = millis();
+
+  if (loop_time - initial_time > led_interval)
+  {
+    initial_time = millis();
+
+    if (led_state)
+    {
+      digitalWrite(LED_PIN, LOW);
+      led_state = false;
+
+      analogWrite(MOTOR_SPEED_1_PIN, 255);
+      digitalWrite(MOTOR_1_PIN, HIGH);
+    }
+    else
+    {
+      digitalWrite(LED_PIN, HIGH);
+      led_state = true;
+
+      analogWrite(MOTOR_SPEED_1_PIN, 255);
+      digitalWrite(MOTOR_1_PIN, LOW);
+    }
+  }
+
+  //float temp = dht.readTemperature();
+  //float humidity = dht.readHumidity();
+
+  //fanVoltage(temp);
 
 }
